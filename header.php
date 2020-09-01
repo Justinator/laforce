92912b86583023c9a0fb79c11fbd8a738f085c98
Justinator <32310311+Justinator@users.noreply.github.com>
2020-08-31 23:08:39 -0500

    updated stylesheet to fix the default YouTube embeds

--- header.php
+++ header.php
@@ -13,24 +13,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
-	<!-- Preconnect to Google Analytics -->
+	<!-- Preconnect to Google Services -->
+	<link rel="dns-prefetch" href="https://google-analytics.com">
 	<link rel="dns-prefetch" href="https://www.google-analytics.com">
 	<link rel="dns-prefetch" href="https://www.googletagmanager.com">
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 	<link href="https://www.google-analytics.com" rel="preconnect" crossorigin>
+	<link href="https://google-analytics.com" rel="preconnect" crossorigin>
 	<link href="https://www.googletagmanager.com" rel="preconnect" crossorigin>
-	<link href="https://fonts.gstatic.com/" rel="preconnect" crossorigin>
+	<link href="https://www.fonts.gstatic.com" rel="preconnect" crossorigin>
 	<link href="/assets/css/wp-content/themes/insightcustom/images/laforce-fav.png" rel="shortcut icon">
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-27521876-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
 	<?php
 
 		if( get_theme_mod( 'header_scripts' ) ) {
